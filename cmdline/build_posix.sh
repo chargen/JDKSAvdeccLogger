c710002e81039b35dc78246fe52dff96401dca11
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-10-07 21:22:08 -0700

    allow build_posix.sh to take parameters for customization

--- cmdline/build_posix.sh
+++ cmdline/build_posix.sh
@@ -1,1 +1,1 @@
-cc  -O2 -DUS_ENABLE_RAW_ETHERNET=1 *.c -I ../common -I ../jdksavdecc-c/include -I ../microsupport/include ../common/*.c ../jdksavdecc-c/src/*.c ../microsupport/src/*.c ../microsupport/src/posix/*.c -lpcap -o jdksavdecc-logger
+cc  "$@" -O2 -DUS_ENABLE_RAW_ETHERNET=1 *.c -I ../common -I ../jdksavdecc-c/include -I ../microsupport/include ../common/*.c ../jdksavdecc-c/src/*.c ../microsupport/src/*.c ../microsupport/src/posix/*.c -lpcap -o jdksavdecc-logger
